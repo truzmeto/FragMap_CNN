514e7da02a7516f0cd3ac88279a4ffe8331c358e
arthdh <arth.dharaskar97@gmail.com>
2019-11-27 18:22:19 -0500

    Fixed naming in frag_names list

--- unit/mapIO.py
+++ unit/mapIO.py
@@ -7,14 +7,14 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.util import pad_map, vec2grid
 from src.mapIO import read_map, write_map
 
-frag_names = ["Benzene", "Propane", "H-bond Donor", "H-bond Acceptor"]
+frag_names = ["Benzene", "Propane", "H-bond Acceptor", "H-bond Donor"]
 
 path_list = ["../data/maps/1ycr.benc.gfe.map",
              "../data/maps/1ycr.prpc.gfe.map",
              "../data/maps/1ycr.hbacc.gfe.map",
              "../data/maps/1ycr.hbdon.gfe.map"]
 
-chan_id = 2 # range 0-3
+chan_id = 0 # range 0-3
 
 ######------------- Test the read_map -----------------#######
 res, n_cells, dens = read_map(path_list[chan_id])
