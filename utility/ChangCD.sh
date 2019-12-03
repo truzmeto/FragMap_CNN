bf736d330940b011996ac7e43adef4d7608c8edc
Siddharth Bhadra-Lobo <sbhadralobo@ucdavis.edu>
2019-12-03 16:36:24 -0500

    Old named script
    not kosher

--- utility/ChangCD.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p tmp
-cp orig_pdbs/*.pdb ./
-for i in $(ls *.pdb); do sed 's/CD\ \ ILE/CD1 ILE/g' $i > tmp/$i; mv tmp/* ./;  done
-for i in $(ls *.pdb); do sed 's/HSD\ /HIS /g' $i > tmp/$i; mv tmp/* ./ ;  done
-for i in $(ls *.pdb); do sed 's/OT1\ LEU/OXT LEU/g' $i > tmp/$i; mv tmp/* ./;  done
-for i in $(ls *.pdb); do sed 's/OT2\ LEU/OXT LEU/g' $i > tmp/$i; mv tmp/* ./;  done
-
