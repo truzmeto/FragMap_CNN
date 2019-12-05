502e56cf285da99a859ec44b4bdf1090dda96609
ArthDh <arth.dharaskar97@gmail.com>
2019-12-05 13:27:39 -0500

    Added test_model function to src/util.py

--- src/util.py
+++ src/util.py
@@ -126,6 +126,32 @@ def box_face_ave(grid):
     return face_ave
 
 
+def test_model(model, saved_weights_path, output_path, output_name, inp_vol):
+    ori = [40.250, -8.472, 20.406] # TODO: Set Origin
+    model.load_state_dict(torch.load(saved_weights_path))
+    model.eval() #Needed to set into inference mode
+    output = model(inp_vol)
+    # TODO: Batch Test output
+    grid = output[0,0,:,:,:].cpu().detach().numpy()
+    grid = unpad_map(grid, xpad = pad[0], ypad = pad[1], zpad = pad[2])
+
+    #convert from Free-E to density 
+    grid[grid <= 0.000] = 0.0001
+    vol = grid #-kBT *np.log(grid)  
+
+    #vol = grid*(gfe_max[i] - gfe_min[i]) + gfe_min[i] 
+    #vol = grid
+    nx, ny, nz = grid.shape
+ 
+    vec = grid2vec([nx,ny,nz], vol)
+    write_map(vec,
+              out_path,
+              out_name,
+              ori = ori,
+              res = resolution,
+              n = [nx,ny,nz])
+
+
 
 if __name__=='__main__':
     print("Universal knowledge must be stored somewhere!")
