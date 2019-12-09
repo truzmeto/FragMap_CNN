Fixed rotation in src/sugment.py

src/augment.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys  
 import os
+import torch
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 '''
@@ -12,7 +13,8 @@ TODO:
 
 def rotate_90(tensor):
 	# Do transpose tensor.transpose(axes1,axes2)
-	return 0
+	temp = torch.rot90(tensor, 1 , [2,3])
+	return temp
 
 def rotate_180(tensor):
 	# Do flip tensor.flip(axes)
@@ -22,20 +24,30 @@ def rotate_270(tensor):
 	# Do 90 + 270 cumulative
 	return 0
 
-def get_random_rotation(tensor):
-	'''
-	Input: tensor
-	Output: Randomly rotated tensor
-	'''
-	# rot = tensor.clone()
-	for i in range(2,5):
-		if np.random.random() > 0.2:
-			print(".", end="")
-			tensor = tensor.flip(i)
-		if np.random.random() > 0.5:
-			print('+',end = "")
-			i = np.random.choice([2,3,4])
-			j = np.random.choice([2,3,4])
-			tensor = tensor.transpose(int(i),int(j))
-	print("")
-	return tensor
+def get_24(tensor):
+	vol_rotated =[]
+	for i in range(-4,4):
+		for j in range(3):
+			if j < 2 :
+				temp = torch.rot90(tensor, i, [j+2, j+3])
+			else:
+				temp = torch.rot90(tensor, i, [j+2, j])
+			vol_rotated.append(temp)
+	return vol_rotated
+# def get_random_rotation(tensor):
+# 	'''
+# 	Input: tensor
+# 	Output: Randomly rotated tensor
+# 	'''
+# 	# rot = tensor.clone()
+# 	for i in range(2,5):
+# 		if np.random.random() > 0.2:
+# 			print(".", end="")
+# 			tensor = tensor.flip(i)
+# 		if np.random.random() > 0.5:
+# 			print('+',end = "")
+# 			i = np.random.choice([2,3,4])
+# 			j = np.random.choice([2,3,4])
+# 			tensor = tensor.transpose(int(i),int(j))
+# 	print("")
+# 	return tensor

unit/augment.py
@@ -9,73 +9,135 @@ import pyvista as pv
 import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.volume import get_volume
-from src.augment import get_random_rotation
-
-
-pdb_ids = ["1pw2", "1ycr","2f6f", "4f5t",
-           "2am9", "3my5_a", "3w8m", "4ic8"] 
-
-path = "../data/"
-path_list = [path+i+".pdb" for i in pdb_ids]
-
-box_size = 60  #prog complains if box_size is float !!!!!! 
-resolution = 1.0
-volume = get_volume(path_list,
-                    box_size,
-                    resolution,
-                    norm = True,
-                    rotate = True)
-
-Agroup_names = ["Sulfur/Selenium"  , "Nitrogen Amide",
-                "Nitrogen Aromatic", "Nitrogen Guanidinium",
-                "Nitrogen Ammonium", "Oxygen Carbonyl",
-                "Oxygen Hydroxyl"  , "Oxygen Carboxyl",
-                "Carbon sp2"       , "Carbon Aromatic",
-                "Carbon sp3"]
-
-p = pv.Plotter(point_smoothing = True, shape=(2, 2))
-fs = 15
-idp = 1
-chan_id = 8 # Atomic group ids, range 0-10
-
-vol1 = volume[idp,chan_id,:,:,:].cpu().numpy()
-text = Agroup_names[chan_id]+" "+pdb_ids[idp]
-p.subplot(0, 0)
-p.add_text(text, position = 'upper_left', font_size = fs)
-p.add_volume(vol1, cmap = "viridis_r", opacity = "linear")
-p.view_xy(negative=False)
+from src.augment import  rotate_90, get_24
 
 
 
-rotated_vol = get_random_rotation(volume)
-vol2 = rotated_vol[idp,chan_id,:,:,:].cpu().numpy()
-text = "Rotated 1 " + Agroup_names[chan_id]+" "+pdb_ids[idp]
-p.subplot(0, 1)
-p.add_text(text, position = 'upper_left', font_size = fs)
-p.add_volume(vol2, cmap = "viridis_r", opacity = "linear")
-p.view_xy(negative=False)
 
+volume = torch.zeros(1,1,55,54,53)
+length = 15
+st = [27,26,25]
 
-rotated_vol_2 = get_random_rotation(volume)
-vol3 = rotated_vol_2[idp,chan_id,:,:,:].cpu().numpy()
-text = "Rotated 2 " + Agroup_names[chan_id]+" "+pdb_ids[idp]
-p.subplot(1, 0)
-p.add_text(text, position = 'upper_left', font_size = fs)
-p.add_volume(vol3, cmap = "viridis_r", opacity = "linear")
-p.view_xy(negative=False)
+volume[:,:,st[0]:st[0]+length,st[1],st[2]] = 0.5
+volume[:,:,st[0]+length:st[0]+length+2,st[1],st[2]] = 0.2
 
+volume[:,:,st[0],st[1]:st[1]+length,st[2]] = 0.5
+volume[:,:,st[0],st[1]+length:st[1]+length+2,st[2]] = 1.
 
-rotated_vol_3 = get_random_rotation(volume)
-vol4 = rotated_vol_3[idp,chan_id,:,:,:].cpu().numpy()
-text = "Rotated 3 " + Agroup_names[chan_id]+" "+pdb_ids[idp]
-p.subplot(1, 1)
-p.add_text(text, position = 'upper_left', font_size = fs)
-p.add_volume(vol4, cmap = "viridis_r", opacity = "linear")
-p.view_xy(negative=False)
+volume[:,:,st[0],st[1],st[2]:st[2]+length] = 0.5
+volume[:,:,st[0],st[1],st[2]+length:st[2]+length+2] = 2.0
+
+volume[:,:,st[0],st[1]:st[1]+length,st[2]:st[2]+length] = 0.2
+volume[:,:,st[0],st[1]+length:st[1]+length+1,st[2]+length:st[2]+length+1] = 1.5
 
 
 
-p.show_axes()
-p.link_views()
-p.show()
+
+t = get_24(volume)
+print(len(t))
+
+# #####################################################################
+
+# p = pv.Plotter(point_smoothing = True, shape=(1, 2))
+# fs = 15
+
+# channel = volume[0,0,:,:,:].cpu().numpy()
+# text = 'original'
+# p.subplot(0, 0)
+# p.add_text(text, position = 'upper_left', font_size = fs)
+# p.add_volume(channel, cmap = "viridis_r", opacity = "linear")
+# #p.add_axes()
+# print(channel.shape)
+
+
+# ###########################################################
+# # #channel = volume_rot[0,0,:,:,:].cpu().numpy()
+# # channel = volume[0,0,:,:,:].cpu()#.numpy()
+# # #channel = channel.transpose(0,2).numpy()
+# # channel = channel.transpose(0, 1).flip(2).numpy()
+# # #channel = channel.flip(2).numpy()
+# #channel = get_random_rotation(channel).numpy()
+
+# volume_rot = rotate_90(volume)
+# channel_2 = volume_rot[0,0,:,:,:].cpu().numpy()
+# print(channel.shape)
+# text = 'rotated'
+# p.subplot(0, 1)
+# p.add_text(text, position = 'upper_left', font_size = fs)
+# p.add_volume(channel_2, cmap = "viridis_r", opacity = "linear")
+# p.add_axes()
+# p.show()
+
+
+
+
+
+
+
+# pdb_ids = ["1pw2", "1ycr","2f6f", "4f5t",
+#            "2am9", "3my5_a", "3w8m", "4ic8"] 
+
+# path = "../data/"
+# path_list = [path+i+".pdb" for i in pdb_ids]
+
+# box_size = 60  #prog complains if box_size is float !!!!!! 
+# resolution = 1.0
+# volume = get_volume(path_list,
+#                     box_size,
+#                     resolution,
+#                     norm = True,
+#                     rotate = True)
+
+# Agroup_names = ["Sulfur/Selenium"  , "Nitrogen Amide",
+#                 "Nitrogen Aromatic", "Nitrogen Guanidinium",
+#                 "Nitrogen Ammonium", "Oxygen Carbonyl",
+#                 "Oxygen Hydroxyl"  , "Oxygen Carboxyl",
+#                 "Carbon sp2"       , "Carbon Aromatic",
+#                 "Carbon sp3"]
+
+# p = pv.Plotter(point_smoothing = True, shape=(2, 2))
+# fs = 15
+# idp = 1
+# chan_id = 8 # Atomic group ids, range 0-10
+
+# vol1 = volume[idp,chan_id,:,:,:].cpu().numpy()
+# text = Agroup_names[chan_id]+" "+pdb_ids[idp]
+# p.subplot(0, 0)
+# p.add_text(text, position = 'upper_left', font_size = fs)
+# p.add_volume(vol1, cmap = "viridis_r", opacity = "linear")
+# p.view_xy(negative=False)
+
+
+
+# rotated_vol = rotate_90(volume)
+# vol2 = rotated_vol[idp,chan_id,:,:,:].cpu().numpy()
+# text = "Rotated 1 " + Agroup_names[chan_id]+" "+pdb_ids[idp]
+# p.subplot(0, 1)
+# p.add_text(text, position = 'upper_left', font_size = fs)
+# p.add_volume(vol2, cmap = "viridis_r", opacity = "linear")
+# p.view_xy(negative=False)
+
+
+# # rotated_vol_2 = get_random_rotation(volume)
+# # vol3 = rotated_vol_2[idp,chan_id,:,:,:].cpu().numpy()
+# # text = "Rotated 2 " + Agroup_names[chan_id]+" "+pdb_ids[idp]
+# # p.subplot(1, 0)
+# # p.add_text(text, position = 'upper_left', font_size = fs)
+# # p.add_volume(vol3, cmap = "viridis_r", opacity = "linear")
+# # p.view_xy(negative=False)
+
+
+# # rotated_vol_3 = get_random_rotation(volume)
+# # vol4 = rotated_vol_3[idp,chan_id,:,:,:].cpu().numpy()
+# # text = "Rotated 3 " + Agroup_names[chan_id]+" "+pdb_ids[idp]
+# # p.subplot(1, 1)
+# # p.add_text(text, position = 'upper_left', font_size = fs)
+# # p.add_volume(vol4, cmap = "viridis_r", opacity = "linear")
+# # p.view_xy(negative=False)
+
+
+
+# p.show_axes()
+# p.link_views()
+# p.show()
 