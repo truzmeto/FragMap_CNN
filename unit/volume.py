e36c173f472c5792f34614d46be84d1fb5b8b073
truzmeto <truzmeto@kent.edu>
2019-12-09 15:56:35 -0500

    changed rotate to rot in volume.py

--- unit/volume.py
+++ unit/volume.py
@@ -11,8 +11,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.volume import get_volume
 
 
-pdb_ids = ["1pw2", "1ycr","2f6f", "4f5t",
-           "2am9", "3my5_a", "3w8m", "4ic8"] 
+pdb_ids = ["1pw2", "1ycr"]#,"2f6f", "4f5t",
+           #"2am9", "3my5_a", "3w8m", "4ic8"] 
 
 path = "../data/"
 path_list = [path+i+".pdb" for i in pdb_ids]
@@ -25,7 +25,7 @@ volume = get_volume(path_list,
                     box_size,
                     resolution,
                     norm = True,
-                    rotate = True)
+                    rot = True)
 
 print(volume.size())
 Agroup_names = ["Sulfur/Selenium"  , "Nitrogen Amide",
@@ -38,7 +38,7 @@ Agroup_names = ["Sulfur/Selenium"  , "Nitrogen Amide",
 p = pv.Plotter(point_smoothing = True, shape=(1, 2))
 fs = 15
 
-idp = 5 
+idp = 0
 chan_id = 1 # Atomic group ids, range 0-10
 vol1 = volume[idp,chan_id,:,:,:].cpu().numpy()
 text = Agroup_names[chan_id]+" "+pdb_ids[idp]
@@ -46,9 +46,10 @@ p.subplot(0, 0)
 p.add_text(text, position = 'upper_left', font_size = fs)
 p.add_volume(vol1, cmap = "viridis_r", opacity = "linear")
 
-chan_id = chan_id 
-vol2 = volume[idp+1,chan_id,:,:,:].cpu().numpy()
-text = Agroup_names[chan_id]+" "+pdb_ids[idp+1]
+chan_id = chan_id +1
+idp=idp+1
+vol2 = volume[idp,chan_id,:,:,:].cpu().numpy()
+text = Agroup_names[chan_id]+" "+pdb_ids[idp]
 p.subplot(0, 1)
 p.add_text(text, position = 'upper_left', font_size = fs)
 p.add_volume(vol2, cmap = "viridis_r", opacity = "linear")
