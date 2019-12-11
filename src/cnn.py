4af704eb569e009e73c042cf3e915bfc0f3de3dd
ArthDh <arth.dharaskar97@gmail.com>
2019-12-11 09:10:40 -0500

    Added a Leaky CNN Model(Testing) + working implementation of rotations(augment)

--- src/cnn.py
+++ src/cnn.py
@@ -10,7 +10,6 @@ def count_parameters(model):
     n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     return n_params
 
-
 class CnnModel(nn.Module):
     def __init__(self, num_input_channels = 11):
         super(CnnModel, self).__init__()
@@ -28,14 +27,14 @@ class CnnModel(nn.Module):
                       kernel_size = k_size,
                       padding = pad),
             nn.BatchNorm3d(48),
-	    nn.ReLU(),
+      nn.ReLU(),
             #nn.LeakyReLU(),
             nn.MaxPool3d(kernel_size = k_size,
                          stride = (1,1,1),
                          padding = pad),
 
             #conv layer 2
-	    nn.Conv3d(in_channels = 48,
+      nn.Conv3d(in_channels = 48,
                       out_channels = 24,
                       kernel_size = k_size,
                       padding = pad),
@@ -72,8 +71,70 @@ class CnnModel(nn.Module):
     def forward(self, input):
         #batch_size = input.size(0)
         conv_out = self.conv(input)
-        return conv_out        
-    
+        return conv_out           
+
+class CnnModel_Leaky(nn.Module):
+    def __init__(self, num_input_channels = 11):
+        super(CnnModel_Leaky, self).__init__()
+
+        #to keep volume dims same
+        s = 3
+        p = (s-1)//2
+        k_size = s #(s,s,s)
+        pad = p #(p,p,p)
+        
+        self.conv = nn.Sequential(
+            #conv layer 1
+            nn.Conv3d(in_channels = num_input_channels,
+                      out_channels = 48, #n convlolutions
+                      kernel_size = k_size,
+                      padding = pad),
+            nn.BatchNorm3d(48),
+            # nn.ReLU(),
+            nn.LeakyReLU(),
+            nn.MaxPool3d(kernel_size = k_size,
+                         stride = (1,1,1),
+                         padding = pad),
+
+            #conv layer 2
+      nn.Conv3d(in_channels = 48,
+                      out_channels = 24,
+                      kernel_size = k_size,
+                      padding = pad),
+            nn.BatchNorm3d(24),
+            # nn.ReLU(),
+            nn.LeakyReLU(),
+            nn.MaxPool3d(kernel_size = k_size,
+                         stride = (1,1,1),
+                         padding = pad),
+            
+            #conv layer 3
+            nn.Conv3d(in_channels = 24,
+                      out_channels = 12,
+                      kernel_size = k_size,
+                      padding = pad),
+            nn.BatchNorm3d(12),
+            # nn.ReLU(),
+            nn.LeakyReLU(),
+            nn.MaxPool3d(kernel_size = k_size,
+                         stride = (1,1,1),
+                         padding = pad),
+
+            #conv layer 4
+            nn.Conv3d(in_channels = 12,
+                      out_channels = 6,
+                      kernel_size = k_size,
+                      padding = pad),
+            nn.BatchNorm3d(6),
+            nn.ReLU()
+            #nn.LeakyReLU()
+        )
+        
+        
+    def forward(self, input):
+        #batch_size = input.size(0)
+        conv_out = self.conv(input)
+        return conv_out
 
 if __name__=='__main__':
     print("Enjoy life!") 
--- train_rot2.py
+++ train_rot2.py
@@ -2,12 +2,12 @@ import os
 import sys
 import torch
 import torch.nn as nn
-from src.cnn  import CnnModel
+from src.cnn  import CnnModel_Leaky
 from src.volume import get_volume
 from src.mapIO import greatest_dim, write_map
 from src.target import get_target
-from src.util import grid2vec, sample_batch, unpad_map
-from src.augment import get_24 
+from src.util import grid2vec, sample_batch, unpad_mapc
+from src.augment import get_24
 import torch.optim as optim
 import numpy as np
 
@@ -16,12 +16,12 @@ import numpy as np
 lrt = 0.0001
 #lrd = 0.0001
 wd = 0.00001
-max_epoch = 100
-batch_size = 2
+max_epoch = 200
+batch_size = 1 #number of structures in a batch
 
 norm = True
 map_norm = True
-nsample = 2 #24
+nsample = 24
 
 #physical params
 resolution = 1.000
@@ -29,34 +29,31 @@ kBT = 0.592 # T=298K, kB = 0.001987 kcal/(mol K)
 
 pdb_path = 'data/'
 # pdb_path = "/scratch/tr443/fragmap/data/"                                                          
-# pdb_ids = ["1ycr"]#, "1pw2", "2f6f", "4f5t", "1s4u", "2am9", "3my5_a", "3w8m"]#,"4ic8"]
-
-pdb_ids = ["1pw2", "2f6f"]
-
+pdb_ids = ["1pw2", "2f6f", "4f5t"]#,"4ic8"]
 
 map_names_list = ["apolar", "hbacc","hbdon", "meoo", "acec", "mamn"]
 map_path = 'data/maps/' 
-#map_path = "/scratch/tr443/fragmap/data/maps/"                                               
+# map_path = "/scratch/tr443/fragmap/data/maps/"                                               
 
-#out_path = '/scratch/tr443/fragmap/output/'
-out_path = 'data/maps/'
+# out_path = '/scratch/tr443/fragmap/output/'
+out_path = 'output/'
 
 dim = greatest_dim(map_path, pdb_ids) + 1
-print("Greatest dim:", dim)
+print(dim)
 box_size = int(dim*resolution)
 params_file_name = 'net_params.pth'
 
 #invoke model
 torch.cuda.set_device(0)
-model = CnnModel().cuda()
+model = CnnModel_Leaky().cuda()
 criterion = nn.MSELoss()
 #criterion = nn.L1Loss()
 optimizer = optim.Adam(model.parameters(), lr = lrt, weight_decay = wd )
 #optimizer = optim.SGD(model.parameters(), lr = lrt, momentum = 0.9)
 
-# model.load_state_dict(torch.load(out_path + params_file_name))
 
-for ibatch in range(nsample):
+for batches in range(nsample):
+
     #sample batch list from all structures
     batch_list, pdb_list = sample_batch(batch_size,
                                         pdb_ids,
@@ -66,62 +63,57 @@ for ibatch in range(nsample):
     volume = get_volume(path_list = batch_list, 
                         box_size = box_size,
                         resolution = resolution,
-                        norm = norm, rot = False)
-
+                        norm = norm,
+                        rot = False)
     
     #get target map tensor
-    target, pad, gfe_min, gfe_max = get_target(map_path,
-                                    map_names_list,
-                                    pdb_ids = pdb_list,
-                                    maxD = dim,
-                                    kBT = kBT,
-                                    cutoff = False,
-                                    density = False,
-                                    map_norm = map_norm)
+    target, pad, gfe_min, gfe_max, ori = get_target(map_path,
+                                                map_names_list,
+                                                pdb_ids = pdb_list,
+                                                maxD = dim,
+                                                kBT = kBT,
+                                                density = False,
+                                                map_norm = map_norm)
+    
+    #convert target maps to torch.cuda
+    target = torch.from_numpy(target).float().cuda()
 
     # Get rotated volume for inputs
     rot_volume = get_24(volume, is_gfe_map = False)
     # Get rotated volume for outputs
-    rot_gfe = get_24(torch.tensor(target), is_gfe_map = True)
+    rot_gfe = get_24(target, is_gfe_map = True)
 
+    #perform forward and backward iterations
     for epoch in range(max_epoch):
-
-        for ii in range(1):
-            #convert target maps to torch.cuda
-            ii_target = rot_gfe[ii].float().cuda()
-            ii_input = rot_volume[ii]      
-            #perform forward and backward iterations
+        for rots in  range(len(rot_volume[:2])):
             optimizer.zero_grad()
-            output = model(ii_input)
-            loss = criterion(output, ii_target)
+            output = model(rot_volume[rots])
+            loss = criterion(output, rot_gfe[rots])
             loss.backward()
             optimizer.step()
+            
+        if epoch % 40 == 0:
+            print('{0},{1},{2}'.format(batches, epoch, loss.item()))
         
-        if epoch % 20 == 0:
-            print('{0},{1},{2}'.format(ibatch, epoch, loss.item()))
-             
 #save trained parameters        
 torch.save(model.state_dict(), out_path+params_file_name)
 
 
-#save density maps to file
-#ori = [23.699, -20.418, 14.198] 
-ori = [40.250, -8.472, 20.406]
 
-for imap in range(len(map_names_list)):
+#for imap in range(len(map_names_list)):
    
-    grid = output[0,imap,:,:,:].cpu().detach().numpy()
-    grid = unpad_map(grid, xpad = dim, ypad = dim, zpad = dim)
+#    grid = output[0,imap,:,:,:].cpu().detach().numpy()
+#    grid = unpad_mapc(grid, pad = pad[0,:].astype(int))
     
-    if map_norm:   # inverse norm
-        vol = grid*(gfe_max[0,imap] - gfe_min[0,imap]) + gfe_min[0,imap] #?????????????????????
-    # else:
-    #     vol = grid
-       
-    nx, ny, nz = vol.shape              #get new dims       
-    vec = grid2vec([nx,ny,nz], vol)     #flatten
+#    if map_norm == True:   # inverse norm
+#        vol = (1.0-grid)*(gfe_max[0,imap] - gfe_min[0,imap]) + gfe_min[0,imap]
+#    else:
+#        vol = grid
+        
+#    nx, ny, nz = vol.shape              #get new dims       
+#    vec = grid2vec([nx,ny,nz], vol)     #flatten
     
     #write frag maps to output file
-    out_name = pdb_ids[0]+"."+ map_names_list[imap]+"_o"
-    write_map(vec, out_path, out_name, ori = ori,
-              res = resolution, n = [nx,ny,nz])
+#    out_name = pdb_ids[0]+"."+ map_names_list[imap]
+#    write_map(vec, out_path, out_name, ori = ori[0,:],
+#              res = resolution, n = [nx,ny,nz])
