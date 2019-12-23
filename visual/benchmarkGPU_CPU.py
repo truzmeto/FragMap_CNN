c8c88a582ac779d30edc191b5bb098d49412360e
Talant Ruzmetov <tr443@guava.ccib.rutgers.edu>
2019-12-23 12:07:39 -0500

    done with gpuVScpu benchmarking

--- visual/benchmarkGPU_CPU.py
+++ visual/benchmarkGPU_CPU.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 ##=====================================================================
 plot_fn= '../figs/numpyVStorchCUDA.pdf'
-fig = plt.figure(figsize=(5.,3.5))
+fig = plt.figure(figsize=(6.,4.))
 
 
 #torch.set_default_tensor_type(torch.cuda.FloatTensor if 
@@ -17,46 +17,48 @@ fig = plt.figure(figsize=(5.,3.5))
 torch.set_default_tensor_type(torch.cuda.FloatTensor) 
 
 
-def tGPU(x,y):
-    
+def tGPU(x,y,n_repeat):
+    n = n_repeat
     x = x.cuda()
     y = y.cuda()
-    n = 10
     t = 0
     for i in range(n):
+        torch.cuda.synchronize()
         start = timeit.default_timer()
-        z = torch.add(x,y)
-        z = torch.exp(z)
-        end = timeit.default_timer() 
+        x = torch.add(x,x)
+        y = torch.exp(y)
+        end = timeit.default_timer()
         t = t + end - start
     return t / n
 
     
-def tCPU(x,y):
+def tCPU(x,y,n_repeat):
     
     x = x.cpu().numpy()
     y = y.cpu().numpy()
-    n = 10
-    t = 0
+    n = n_repeat
+    start = timeit.default_timer()
     for i in range(n):
-        start = timeit.default_timer()
-        z = np.exp(x + y)
-        end = timeit.default_timer()
-        t = t + end - start
+        x = x + x
+        y = np.exp(y)
+
+    end = timeit.default_timer()
+    t = end - start
     return  t / n
     
 
-delta = 4
+delta = 5
 cpu2gpu = []
 boxDim = []
-
-for d in range(60, 106, delta):
+n = 20
+for d in range(10, 110, delta):
     
     boxDim.append(d)
-    x = torch.rand(3, 11, d, d, d)
-    y = torch.rand(3, 11, d, d, d)
-    t_cpu = tCPU(x,y)
-    t_gpu = tGPU(x,y)
+    x = torch.rand(4, 11, d, d, d)
+    y = torch.rand(4, 6, d, d, d)
+    
+    t_cpu = tCPU(x,y,n)
+    t_gpu = tGPU(x,y,n)
     cpu2gpu.append(t_cpu/t_gpu)
 
 
